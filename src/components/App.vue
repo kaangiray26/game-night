0dcae00d14770ef2a9ef20cb7b99ba26374fb2d6
Kaan Giray Buzluk <kaangiray26@protonmail.com>
2024-02-21 14:00:20 +0100

    added some games

---
+++ src/components/App.vue
@@ -0,0 +1,82 @@
+<template>
+    <div class="container py-4">
+        <div class="d-flex align-items-center mb-3">
+            <h1 class="material-symbols-outlined mb-0 pe-1">
+                gamepad
+            </h1>
+            <h1 class="fw-bold mb-0">Game Night</h1>
+        </div>
+        <div class="d-flex flex-column bg-tertiary rounded p-3 mb-3">
+            <h2>Video call</h2>
+            <p>Choose a platform to call your friends:</p>
+            <div class="row g-3">
+                <div class="col-auto">
+                    <div class="item">
+                        <img class="item-img" src="/images/skype.svg">
+                        <h3 class="item-title">Skype</h3>
+                        <a href="https://web.skype.com/" target="_blank" class="stretched-link"></a>
+                    </div>
+                </div>
+                <div class="col-auto">
+                    <div class="item">
+                        <img class="item-img" src="/images/zoom.svg">
+                        <h3 class="item-title">Zoom</h3>
+                        <a href="https://zoom.us/join/" target="_blank" class="stretched-link"></a>
+                    </div>
+                </div>
+                <div class="col-auto">
+                    <div class="item">
+                        <img class="item-img" src="/images/jitsi.svg">
+                        <h3 class="item-title">Jitsi</h3>
+                        <a href="https://meet.jit.si/" target="_blank" class="stretched-link"></a>
+                    </div>
+                </div>
+                <div class="col-auto">
+                    <div class="item">
+                        <img class="item-img" src="/images/discord.svg">
+                        <h3 class="item-title">Discord</h3>
+                        <a href="https://discord.com/app" target="_blank" class="stretched-link"></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="d-flex flex-column bg-tertiary rounded p-3 mb-3">
+            <h2>Games</h2>
+            <p>Here are some games you can play with your friends:</p>
+            <div class="row">
+                <div class="col-auto">
+                    <div class="item">
+                        <img class="item-img" src="/images/scrabble.png">
+                        <h3 class="item-title">Scrabble</h3>
+                        <a href="https://playscrabble.com/" target="_blank" class="stretched-link"></a>
+                    </div>
+                </div>
+                <div class="col-auto">
+                    <div class="item">
+                        <img class="item-img" src="/images/bananagrams.webp">
+                        <h3 class="item-title">Bananagrams</h3>
+                        <a href="https://www.playbananagrams.com/" target="_blank" class="stretched-link"></a>
+                    </div>
+                </div>
+                <div class="col-auto">
+                    <div class="item">
+                        <img class="item-img" src="/images/backgammon.svg">
+                        <h3 class="item-title">Backgammon</h3>
+                        <a href="https://www.crazygames.com/game/classic-backgammon" target="_blank"
+                            class="stretched-link"></a>
+                    </div>
+                </div>
+                <div class="col-auto">
+                    <div class="item">
+                        <img class="item-img" src="/images/chess.png">
+                        <h3 class="item-title">Chess</h3>
+                        <a href="https://www.chess.com/" target="_blank" class="stretched-link"></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+</script>
