c34e6e1dedec741b7cdfe6410987909a0b059fb9
Kaan Giray Buzluk <kaangiray26@protonmail.com>
2024-02-21 17:14:13 +0100

    added some games

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,11 +1,12 @@
 <template>
     <div class="container py-4">
-        <div class="d-flex align-items-center mb-3">
+        <div class="d-flex align-items-center mb-1">
             <h1 class="material-symbols-outlined mb-0 pe-1">
                 gamepad
             </h1>
             <h1 class="fw-bold mb-0">Game Night</h1>
         </div>
+        <p class="mb-3">2 player online games for couples, friends, and family.</p>
         <div class="d-flex flex-column bg-tertiary rounded p-3 mb-3">
             <h2>Video call</h2>
             <p>Choose a platform to call your friends:</p>
@@ -13,29 +14,36 @@
                 <div class="col-auto">
                     <div class="item">
                         <img class="item-img" src="/images/skype.svg">
-                        <h3 class="item-title">Skype</h3>
+                        <span class="item-title">Skype</span>
                         <a href="https://web.skype.com/" target="_blank" class="stretched-link"></a>
                     </div>
                 </div>
                 <div class="col-auto">
                     <div class="item">
                         <img class="item-img" src="/images/zoom.svg">
-                        <h3 class="item-title">Zoom</h3>
+                        <span class="item-title">Zoom</span>
                         <a href="https://zoom.us/join/" target="_blank" class="stretched-link"></a>
                     </div>
                 </div>
                 <div class="col-auto">
                     <div class="item">
                         <img class="item-img" src="/images/jitsi.svg">
-                        <h3 class="item-title">Jitsi</h3>
+                        <span class="item-title">Jitsi</span>
                         <a href="https://meet.jit.si/" target="_blank" class="stretched-link"></a>
                     </div>
                 </div>
                 <div class="col-auto">
                     <div class="item">
                         <img class="item-img" src="/images/discord.svg">
-                        <h3 class="item-title">Discord</h3>
+                        <span class="item-title">Discord</span>
                         <a href="https://discord.com/app" target="_blank" class="stretched-link"></a>
+                    </div>
+                </div>
+                <div class="col-auto">
+                    <div class="item">
+                        <img class="item-img" src="/images/messenger.svg">
+                        <span class="item-title">Messenger</span>
+                        <a href="https://messenger.buzl.uk/" target="_blank" class="stretched-link"></a>
                     </div>
                 </div>
             </div>
@@ -43,25 +51,25 @@
         <div class="d-flex flex-column bg-tertiary rounded p-3 mb-3">
             <h2>Games</h2>
             <p>Here are some games you can play with your friends:</p>
-            <div class="row">
+            <div class="row g-3">
                 <div class="col-auto">
                     <div class="item">
                         <img class="item-img" src="/images/scrabble.png">
-                        <h3 class="item-title">Scrabble</h3>
+                        <span class="item-title">Scrabble</span>
                         <a href="https://playscrabble.com/" target="_blank" class="stretched-link"></a>
                     </div>
                 </div>
                 <div class="col-auto">
                     <div class="item">
                         <img class="item-img" src="/images/bananagrams.webp">
-                        <h3 class="item-title">Bananagrams</h3>
+                        <span class="item-title">Bananagrams</span>
                         <a href="https://www.playbananagrams.com/" target="_blank" class="stretched-link"></a>
                     </div>
                 </div>
                 <div class="col-auto">
                     <div class="item">
                         <img class="item-img" src="/images/backgammon.svg">
-                        <h3 class="item-title">Backgammon</h3>
+                        <span class="item-title">Backgammon</span>
                         <a href="https://www.crazygames.com/game/classic-backgammon" target="_blank"
                             class="stretched-link"></a>
                     </div>
@@ -69,14 +77,81 @@
                 <div class="col-auto">
                     <div class="item">
                         <img class="item-img" src="/images/chess.png">
-                        <h3 class="item-title">Chess</h3>
+                        <span class="item-title">Chess</span>
                         <a href="https://www.chess.com/" target="_blank" class="stretched-link"></a>
+                    </div>
+                </div>
+                <div class="col-auto">
+                    <div class="item">
+                        <img class="item-img" src="/images/skribble.png">
+                        <span class="item-title">skribbl.io</span>
+                        <a href="https://skribbl.io/" target="_blank" class="stretched-link"></a>
+                    </div>
+                </div>
+                <div class="col-auto">
+                    <div class="item">
+                        <img class="item-img" src="/images/battleship.png">
+                        <span class="item-title">Battleship</span>
+                        <a href="https://battleship-game.org/en/" target="_blank" class="stretched-link"></a>
+                    </div>
+                </div>
+                <div class="col-auto">
+                    <div class="item">
+                        <img class="item-img" src="/images/gartic.jpg">
+                        <span class="item-title">Gartic.io</span>
+                        <a href="https://gartic.io/" target="_blank" class="stretched-link"></a>
+                    </div>
+                </div>
+                <div class="col-auto">
+                    <div class="item">
+                        <img class="item-img" src="/images/supertuxkart.png">
+                        <span class="item-title">SuperTuxKart</span>
+                        <a href="https://supertuxkart.net/Download" target="_blank" class="stretched-link"></a>
+                    </div>
+                </div>
+                <div class="col-auto">
+                    <div class="item">
+                        <img class="item-img" src="/images/codenames.webp">
+                        <span class="item-title">Codenames: Duet</span>
+                        <a href="https://codenames.game/" target="_blank" class="stretched-link"></a>
+                    </div>
+                </div>
+                <div class="col-auto">
+                    <div class="item">
+                        <img class="item-img" src="/images/8ballpool.png">
+                        <span class="item-title">8 Ball Pool</span>
+                        <a href="https://8ballpool.com/en/game" target="_blank" class="stretched-link"></a>
+                    </div>
+                </div>
+                <div class="col-auto">
+                    <div class="item">
+                        <img class="item-img" src="/images/club_penguin.jpg">
+                        <span class="item-title">Club Penguin</span>
+                        <a href="https://newcp.net" target="_blank" class="stretched-link"></a>
+                    </div>
+                </div>
+                <div class="col-auto">
+                    <div class="item">
+                        <img class="item-img" src="/images/stardew_valley.png">
+                        <span class="item-title">Stardew Valley</span>
+                        <a href="steam://launch/413150/Dialog" target="_blank" class="stretched-link"></a>
+                    </div>
+                </div>
+                <div class="col-auto">
+                    <div class="item">
+                        <img class="item-img" src="/images/it_takes_two.webp">
+                        <span class="item-title">It Takes Two</span>
+                        <a href="steam://launch/1426210/Dialog" target="_blank" class="stretched-link"></a>
+                    </div>
+                </div>
+                <div class="col-auto">
+                    <div class="item">
+                        <img class="item-img" src="/images/overcooked_2.webp">
+                        <span class="item-title">Overcooked! 2</span>
+                        <a href="steam://launch/728880/Dialog" target="_blank" class="stretched-link"></a>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </template>
-
-<script setup>
-</script>
